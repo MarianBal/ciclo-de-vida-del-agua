bff3096e15ee3363f25a572e7e8b1f220bb75b89
MarianBal <mariana_balsamo@hotmail.com>
2023-11-03 17:29:35 -0300

    create app

---
+++ src/components/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/style.css'
+
+const Login = ()=> {
+  return (
+    <div className="background-login">
+      <div className="titulo-principal">
+        Ciclo de Vida del Agua
+       </div>
+    </div>
+  )
+}
+
+export default Login;
