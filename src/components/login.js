3e9b9417f50328a20bab0e3ced93d74a20c90b5e
MarianBal <mariana_balsamo@hotmail.com>
2023-11-04 14:42:25 -0300

    add inputs

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import '../css/style.css'
 
 const Login = ()=> {
   return (
     <div className="background-login">
-      <div className="titulo-principal">
+      <div className="main-title">
         Ciclo de Vida del Agua
+       </div>
+       <div className="input-wrapper">
+         <TextField id="name" label="Nombre y Apellido" variant="outlined" />
+            <TextField id="mail" label="Mail" variant="outlined" />
        </div>
     </div>
   )
